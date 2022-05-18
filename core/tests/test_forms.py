0a46859e88298d03fcf7140c211ed3a4ddf55bfc
Diego Carlos da Silva <diego_carlos110@hotmail.com>
2022-05-18 13:52:21 -0300

    FormTests completo 100%

--- core/tests/test_forms.py
+++ core/tests/test_forms.py
@@ -1,32 +1,35 @@
 from django.test import TestCase
+
 from core.forms import ContatoForm
 
+
 class ContatoFormTestCase(TestCase):
-	
-	def setUp(self):
-		self.nome = 'Diego Carlos'
-		self.email = 'dcarlos@gmaial.com'
-		self.assunto = 'Um assunto qualquer'
-		self.mensagem = 'uma mensagem qualquer'
+
+    def setUp(self):
+        self.nome = 'Felicity Jones'
+        self.email = 'felicity@gmail.com'
+        self.assunto = 'Um assunto qualquer'
+        self.mensagem = 'Uma mensagem qualquer'
+
+        self.dados = {
+            'nome': self.nome,
+            'email': self.email,
+            'assunto': self.assunto,
+            'mensagem': self.mensagem
+        }
+
+        self.form = ContatoForm(data=self.dados)  # ContatoForm(request.POST)
+
 		
-		self.dados = {
-			'nome' : self.nome,
-			'email' : self.email,
-			'assunto' : self.assunto,
-			'mensagem' : self.mensagem
-		}
 		
-		self.form = ContatoForm(data=self.dados) # mesma coisa ue fazer na vies ContatoForm(request.POST)
 		
-		def test_enviar_email(self):
-			form1 = ContatoForm(data=self.dados)
-			form1.is_valid()
-			res1 = form1.enviar_email()
-			
-			form2 = self.form
-			form2.is_valid()
-			res2 = form2.enviar_email()
-			
-			
-			
-			self.assertEquals(res1, res2)
+    def test_s_mail(self):
+        form1 = ContatoForm(data=self.dados)
+        form1.is_valid()
+        res1 = form1.send_mail()	
+		
+        form2 = self.form
+        form2.is_valid()
+        res2 = form2.send_mail()
+
+        self.assertEquals(res1, res2)
